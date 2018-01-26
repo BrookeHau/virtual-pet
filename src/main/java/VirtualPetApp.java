6f7c60c64ae2525e9961b2b800a78890ea589985
BrookeHau <brookerhauser@gmail.com>
2018-01-26 15:10:55 -0500

    Updated with game over option

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -18,38 +18,54 @@ public class VirtualPetApp {
 		System.out.println("Happiness level is: " + myPet.getHappiness());
 		System.out.println();
 
-		// while (myPet.getBoredom() > 3) {
-		for (int i = 0; i < 3; i++) {
+		while (myPet.getBoredom() < 10 ) {
+			for (int i = 0; i < 3; i++) {
 
-			System.out.println("What would you like to do?");
-			System.out.println("1. Feed your pet");
-			System.out.println("2. Water your pet");
-			System.out.println("3. Play with your pet");
-			System.out.println("4. Let your pet sleep");
-			System.out.println("5. Do nothing");
-			String optionEntered = input.nextLine();
+				System.out.println("What would you like to do?");
+				System.out.println("1. Feed your pet");
+				System.out.println("2. Water your pet");
+				System.out.println("3. Play with your pet");
+				System.out.println("4. Let your pet sleep");
+				System.out.println("5. Do nothing");
+				String optionEntered = input.nextLine();
 
-			if (optionEntered.equals("1")) {
-				System.out.println("You fed " + petName);
-				myPet.feedPet(2);
+				if (optionEntered.equals("1")) {
+					System.out.println("You fed " + petName);
+					myPet.feedPet(2);
 
+				}
+				if (optionEntered.equals("2")) {
+					System.out.println("You watered " + petName);
+					myPet.waterPet(1);
+				}
+				if (optionEntered.equals("3")) {
+					System.out.println("You played with " + petName);
+					myPet.playWithPet(4);
+				}
+				if (optionEntered.equals("4")) {
+					System.out.println("Your pet is now sleeping");
+					myPet.letPetSleep(3);
+				}
+				if (optionEntered.equals("5")) {
+					System.out.println("You did nothing, awful parenting");
+					myPet.doNothing(4);
+				}
+				if (myPet.getBoredom()>10){
+					System.out.println("Game over! Boredom is greater than 10");
+					System.exit(0);
+				}
+				System.out.println();
+				System.out.println("Your Pet Stats are the following: ");
+				System.out.println("Hunger level is: " + myPet.getHunger());
+				System.out.println("Thirst level is: " + myPet.getThirst());
+				System.out.println("Boredom level is: " + myPet.getBoredom());
+				System.out.println("Sleepiness level is: " + myPet.getSleepiness());
+				System.out.println("Happiness level is: " + myPet.getHappiness());
+				System.out.println();
 			}
-			if (optionEntered.equals("2")) {
-				System.out.println("You watered " + petName);
-				myPet.waterPet(1);
-			}
-			if (optionEntered.equals("3")) {
-				System.out.println("You played with " + petName);
-				myPet.playWithPet(4);
-			}
-			if (optionEntered.equals("4")) {
-				System.out.println("Your pet is now sleeping");
-				myPet.letPetSleep(3);
-			}
-			if (optionEntered.equals("5")) {
-				System.out.println("You did nothing, awful parenting");
-				myPet.doNothing(4);
-			}
+
+			System.out.println("Today is a new day");
+			myPet.tick();
 			System.out.println();
 			System.out.println("Your Pet Stats are the following: ");
 			System.out.println("Hunger level is: " + myPet.getHunger());
@@ -59,20 +75,6 @@ public class VirtualPetApp {
 			System.out.println("Happiness level is: " + myPet.getHappiness());
 			System.out.println();
 		}
-
-		System.out.println("Today is a new day");
-		myPet.tick();
-		System.out.println();
-		System.out.println("Your Pet Stats are the following: ");
-		System.out.println("Hunger level is: " + myPet.getHunger());
-		System.out.println("Thirst level is: " + myPet.getThirst());
-		System.out.println("Boredom level is: " + myPet.getBoredom());
-		System.out.println("Sleepiness level is: " + myPet.getSleepiness());
-		System.out.println("Happiness level is: " + myPet.getHappiness());
-		System.out.println();
-
-		// System.out.println("Game Over! Boredom level was less than 3.");
-		// System.exit(0);
 	}
 
 }
